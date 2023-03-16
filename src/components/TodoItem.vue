9aecdd07789a5bc564b9e86263f66bb312b5347c
codedbychavez <codedbychavez@gmail.com>
2023-03-16 19:15:44 -0300

    Updates

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="todo-item">TODO Item</div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+  },
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* Styles for the Todo item */
+</style>
