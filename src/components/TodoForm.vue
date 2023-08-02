fa9ab9822f6d58e72e057c422c1c1fc4010a0869
Chavez Harris <codedbychavez@gmail.com>
2023-08-02 13:17:37 -0400

    Completed type improvements

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,15 +1,10 @@
 <template>
-  <form ref="todoForm" @submit.prevent="handleFormSubmit" class="todo-form">
+  <form ref="todoForm" @submit.prevent="handleFormSubmit()" class="todo-form">
     <div class="form-element">
-      <input v-model="isComplete" type="checkbox" />
+      <input v-model="todo.status" type="checkbox" />
     </div>
     <div class="form-element">
-      <input
-        v-model="title"
-        class="title-input"
-        type="text"
-        placeholder="title"
-      />
+      <input v-model="todo.title" class="title-input" type="text" placeholder="title" />
     </div>
     <div class="form-element">
       <button class="submit-button" type="submit">Add</button>
@@ -17,39 +12,34 @@
   </form>
 </template>
 
-<script>
+<script lang="ts" setup>
 import { v4 as uuid } from "uuid";
+import { Todo } from "../Models";
+import { reactive } from "vue";
 
-class Todo {
-  constructor(id, title, isComplete) {
-    this.id = id;
-    this.title = title;
-    this.isComplete = isComplete;
-  }
+const emits = defineEmits(['addTodo']);
+
+const todo: Todo = reactive({ id: "", title: "", status: false });
+
+const generateRandomId = (): string => {
+  return uuid();
+};
+
+const resetForm = (): void => {
+  todo.id = "",
+    todo.title = "",
+    todo.status = false
 }
 
-export default {
-  emits: ["addTodo"],
-  data() {
-    return {
-      id: "",
-      title: "",
-      isComplete: false,
-    };
-  },
-  methods: {
-    generateRandomId() {
-      return uuid();
-    },
 
-    handleFormSubmit() {
-      this.id = this.generateRandomId();
-      const todo = new Todo(this.id, this.title, this.isComplete);
-      this.$emit("addTodo", todo);
-      this.$refs.todoForm.reset();
-    },
-  },
+const handleFormSubmit = () => {
+  const generatedTodoId = generateRandomId();
+  const todoToAdd: Todo = { id: generatedTodoId, title: todo.title, status: todo.status };
+  emits('addTodo', todoToAdd)
+  resetForm();
 };
+
+
 </script>
 
 <style>
